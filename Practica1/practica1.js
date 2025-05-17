67c6f1b28bbfeb318c42df8ae893627a6a53754f
Jesuscruz77 <146015988+Jesuscruz77@users.noreply.github.com>
2025-05-16 18:32:02 -0600

    Ejercicio 3
    Se definió una arrow function llamada Saludo que recibe dos parámetros de nombre y edad para después con un console.log mandar a llamar la función una vez para poder ver el resultado esperado.

--- Practica1/practica1.js
+++ Practica1/practica1.js
@@ -15,3 +15,11 @@ function cuadrado(numero) {
 console.log(cuadrado(3)); 
 console.log(cuadrado(5));   
 console.log(cuadrado(100)); 
+
+//Ejercicio 3
+
+const Saludo = (nombre, edad) => {
+    return "Hola, " + nombre + " y tengo "+ edad + "años.";
+};
+
+console.log(Saludo("Isay", 37));
