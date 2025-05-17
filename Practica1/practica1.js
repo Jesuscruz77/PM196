97766e8be7591f7a13995e1b1ab07ba37dfcbc25
Jesuscruz77 <146015988+Jesuscruz77@users.noreply.github.com>
2025-05-16 18:02:10 -0600

    Ejercicio 2

--- Practica1/practica1.js
+++ Practica1/practica1.js
@@ -8,4 +8,10 @@ const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
 
 console.log(saludo);
 
-
+//Ejercicio 2
+function cuadrado(numero) {
+    return numero * numero;
+}
+console.log(cuadrado(3)); 
+console.log(cuadrado(5));   
+console.log(cuadrado(100)); 
