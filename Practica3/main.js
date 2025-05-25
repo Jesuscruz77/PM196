a62a18386f517f059b04d4d0c0e4c6685369d492
Jesuscruz77 <146015988+Jesuscruz77@users.noreply.github.com>
2025-05-24 20:07:22 -0600

    Ejercicio 3
    se creo una función para obtener datos usando async/await

--- Practica3/main.js
+++ Practica3/main.js
@@ -18,4 +18,25 @@ verificarUsuario("admin")
 .catch(err => console.error(err));
 verificarUsuario("lvan")
 .then(res => console.log(res))
-.catch(err => console.error("Ivan = " + err)); // Acceso denegado 
+.catch(err => console.error("Ivan = " + err)); // Acceso denegado
+
+// Crea una función obtenerDatos() que simule una llamada a una API con setTimeout y
+// usar async/await para esperar el resultado.
+
+function simularPeticionAPI() {
+    return new Promise(resolve => {
+    setTimeout(() => {
+    resolve("Datos recibidos correctamente");
+    }, 5000);
+    });
+}
+
+async function obtenerDatos() {
+    // Usa await para esperar la promesa de simularPeticion
+    // Imprime el resultado
+    const resultado = await simularPeticionAPI();
+    console.log(resultado);
+}
+
+//Usa la funcion async
+obtenerDatos();
