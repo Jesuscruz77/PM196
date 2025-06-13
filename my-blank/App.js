86e768ab9ddba56be8470fad155c61074a9253ce
Jesuscruz77 <146015988+Jesuscruz77@users.noreply.github.com>
2025-06-13 14:30:47 -0600

    componente Texto propio con su propiedad

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,19 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-const Texto = ()=> {
+const Texto = (props)=> {
+    const {contenido} = props;
       return(
-        <Text>Hola mundo react Native</Text>
+        <Text>{contenido}</Text>
       );
     };
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto></Texto>
-      <Texto></Texto>
-      <Texto></Texto>
-      <Texto></Texto>
+      <Texto contenido = "Holaaaaaaaaaaaaaaaaaaaaaaaaa"></Texto>
+      <Texto contenido = "Hola desde un parametro"></Texto>
+      <Texto contenido = "React Native"></Texto>
       <Button title='Presioname'> </Button>,
       <StatusBar style="auto" />
     </View>
