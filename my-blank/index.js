6e32240134fb45dc06d200eb1da92bdd0848c54f
Jesuscruz77 <146015988+Jesuscruz77@users.noreply.github.com>
2025-07-01 15:35:14 -0600

    Practica 13
    practica con FlatList & SectionList

--- my-blank/index.js
+++ my-blank/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './practica12';
+import App from './practica13';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
